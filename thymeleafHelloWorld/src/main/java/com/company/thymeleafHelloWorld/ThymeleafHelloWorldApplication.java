1b609a84b622c6d4aac3ef13655f9e1cca167974
bryanmax9 <brillant16.gcv@gmail.com>
2023-02-14 22:59:41 -0800

    my first tthymeleaf!

---
+++ thymeleafHelloWorld/src/main/java/com/company/thymeleafHelloWorld/ThymeleafHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.company.thymeleafHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafHelloWorldApplication.class, args);
+	}
+
+}
